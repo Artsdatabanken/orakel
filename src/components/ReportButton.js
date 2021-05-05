159dbf651d5c21fef210dd3202c8fbd73a8310f6
Wouter <wouter.koch@artsdatabanken.no>
2021-05-05 16:55:22 +0200

    Upload img first to prevent popup block on report

--- src/components/ReportButton.js
+++ src/components/ReportButton.js
@@ -9,9 +9,10 @@ import Button from "@material-ui/core/Button";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContentText from "@material-ui/core/DialogContentText";
 
-
 function ReportButton({ reportResult, croppedImages }) {
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [encryptionData, setEncryptionData] = useState(false);
+  const [uploadingImages, setUploadingImages] = useState(false);
 
   const handleDialogClose = () => {
     setDialogOpen(false);
@@ -19,54 +20,54 @@ function ReportButton({ reportResult, croppedImages }) {
 
   let device = { platform: "app" };
 
-  const reportAO = () => {
-    window.open(makeURL(reportResult), "_blank");
-    setDialogOpen(false);
-  };
-
   const openDialog = (e) => {
-    setDialogOpen(true);
+    if (!runningOnMobile()) {
+      saveImages(croppedImages);
+    } else {
+      setDialogOpen(true);
+    }
   };
 
-  function makeURL(){
+  function makeURL() {
     let prefix =
-    window.location.hostname === "orakel.test.artsdatabanken.no"
-      ? "test"
-      : "www";
-    
+      window.location.hostname === "orakel.test.artsdatabanken.no"
+        ? "test"
+        : "www";
+
     let probability = Math.round(reportResult.probability * 100);
     let percentage = `%7Cpercentage%3D${probability}`;
     let url = `https://${prefix}.artsobservasjoner.no/SubmitSighting/`;
-    let from = 'meta=from%3Dorakel%7C';
+    let from = "meta=from%3Dorakel%7C";
     let platform = `platform%3Ddesktopweb`;
     let reporttype = `Report?`;
-  
-  if (runningOnMobile()) {
-    // TODO: Await update from artsobsmobile before update, to ensure we don't redirect user to nonexistent page
-    platform = `platform%3D${window.cordova ? (device ? device.platform : "app") : "mobileweb"}`;
-    reporttype = `?scientificname=${reportResult.taxon.scientificNameID}%26`;      
-    if (prefix === "test") {
-      url = 'https://utv.artsdatabanken.no/a2m/#/';
-    }else if(window.location.hostname === 'localhost'){
-      // When testing with artsobs mobile, run artsobsmobile on localhost:3000 
-      // And run this on any other port. Artsobs mobile has issues when run on other ports.
-      // url= 'http://localhost:3000/#/'      
-      url = 'https://utv.artsdatabanken.no/a2m/#/';
-    }else{
-      url = 'https://mobil.artsobservasjoner.no/#/';
-    }
-    // REDIRECT TO THIS ONE
-    // url += "report";
-    url += "orakel";
 
-  } else if (reportResult.taxon.scientificNameID) {
-    reporttype = `ReportByScientificName/${reportResult.taxon.scientificNameID}?`;
-  }           
-  return url+reporttype+from+platform+percentage;
+    if (runningOnMobile()) {
+      // TODO: Await update from artsobsmobile before update, to ensure we don't redirect user to nonexistent page
+      platform = `platform%3D${
+        window.cordova ? (device ? device.platform : "app") : "mobileweb"
+      }`;
+      reporttype = `?scientificname=${reportResult.taxon.scientificNameID}%26`;
+      if (prefix === "test") {
+        url = "https://utv.artsdatabanken.no/a2m/#/";
+      } else if (window.location.hostname === "localhost") {
+        // When testing with artsobs mobile, run artsobsmobile on localhost:3000
+        // And run this on any other port. Artsobs mobile has issues when run on other ports.
+        // url= 'http://localhost:3000/#/'
+        url = "https://utv.artsdatabanken.no/a2m/#/";
+      } else {
+        url = "https://mobil.artsobservasjoner.no/#/";
+      }
+      // REDIRECT TO THIS ONE
+      // url += "report";
+      url += "orakel";
+    } else if (reportResult.taxon.scientificNameID) {
+      reporttype = `ReportByScientificName/${reportResult.taxon.scientificNameID}?`;
+    }
+    return url + reporttype + from + platform + percentage;
   }
 
-  
-  function saveImages(croppedImages){
+  function saveImages(croppedImages) {
+    setUploadingImages(true);
     // Only use if on mobile for now
     var formdata = new FormData();
     for (let image of croppedImages) {
@@ -75,29 +76,35 @@ function ReportButton({ reportResult, croppedImages }) {
     let url = "https://ai.artsdatabanken.no";
     // url = "http://localhost:5000"; // For testing the ai server script locally
     axios
-      .post(url+"/save", formdata)
+      .post(url + "/save", formdata)
       .then((res) => {
-        console.log("result from posting", res.data)
-        redirectToArtsObs(res.data);
+        setEncryptionData(res.data);
+        setDialogOpen(true);
+        setUploadingImages(false);
       })
       .catch((error) => {
-        console.log("error")
+        console.log("error");
       });
-  };
+  }
 
-  function redirectToArtsObs(data){
-    let id = data.id;
-    let password = data.password;
+  function redirectToArtsObs() {
     let url = makeURL(reportResult);
-    url += "&id="+id+"&password="+password;
+    if (encryptionData) {
+      let id = encryptionData.id;
+      let password = encryptionData.password;
+      url += "&id=" + id + "&password=" + password;
+    }
     window.open(url, "_blank");
     setDialogOpen(false);
   }
 
-
   return (
     <React.Fragment>
-      <div className="btn primary" onClick={openDialog.bind(this)}>Rapporter funn</div>
+      <div className="btn primary" onClick={openDialog.bind(this)}>
+        Rapporter funn
+        {uploadingImages && <span className="littleSpinner"></span>}
+      </div>
+
       <Dialog
         onClose={handleDialogClose}
         aria-labelledby="dialog-title"
@@ -113,29 +120,21 @@ function ReportButton({ reportResult, croppedImages }) {
             feil også ved høy treffprosent. Vil du fortsette?
           </DialogContentText>
 
-          <form 
-          //onSubmit={e => handleSubmit(e)}
-          action="" method="post" enctype="multipart/form-data">
-          </form>
+          <form action="" method="post" enctype="multipart/form-data"></form>
         </DialogContent>
         <DialogActions>
-        <Button onClick={handleDialogClose} color="primary">
+          <Button onClick={handleDialogClose} color="primary">
             Avbryt
           </Button>
-        <Button  
-           onClick={e => 
-            {if (runningOnMobile()){
-              saveImages(croppedImages)
-            }else{
-              reportAO()
-            }} 
-          }         
-          color="primary" autoFocus>
+          <Button
+            onClick={(e) => {
+              redirectToArtsObs();
+            }}
+            color="primary"
+            autoFocus
+          >
             Fortsett
           </Button>
-         
-
-          
         </DialogActions>
       </Dialog>
     </React.Fragment>
